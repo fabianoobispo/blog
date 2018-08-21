8e781db7c1afd5d6325479e58fa6b17ea97ab974
fbc623 <fbc623@gmail.com>
2018-08-20 22:10:41 -0300

    primeira

---
+++ application/controllers/Categorias.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Categorias extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+
+		$this->load->model('categorias_model','modelcategorias');
+		$this->categorias = $this->modelcategorias->listar_categorias();
+
+		
+
+	}
+
+	public function index($id, $slug=null)
+	{
+
+		$dados['categorias'] = $this->categorias;
+		$this->load->model('publicacoes_model','modelpublocacoes');
+		$dados['postagem'] = $this->modelpublocacoes->categoria_pub($id);
+
+
+
+		//dados a serem enviados para o cabeçalho
+		$dados['titulo'] = 'Categorias';
+		$dados['subtitulo'] = '';
+		$dados['subtitulodb'] = $this->modelcategorias->listar_titulo($id);
+
+
+
+		$this->load->view('frontend/template/html-header',$dados);
+		$this->load->view('frontend/template/header');
+		$this->load->view('frontend/categoria');
+		$this->load->view('frontend/template/aside');
+		$this->load->view('frontend/template/footer');
+		$this->load->view('frontend/template/html-footer');
+	}
+
+	
+}
---
+++ application/controllers/Home.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+
+		$this->load->model('categorias_model','modelcategorias');
+		$this->categorias = $this->modelcategorias->listar_categorias();
+
+		
+
+	}
+
+	public function index()
+	{
+
+		$dados['categorias'] = $this->categorias;
+		$this->load->model('publicacoes_model','modelpublocacoes');
+		$dados['postagem'] = $this->modelpublocacoes->destaques_home();
+
+
+
+		//dados a serem enviados para o cabeçalho
+		$dados['titulo'] = 'Pagina Inicial';
+		$dados['subtitulo'] = 'Postagens Recentes';
+
+		$this->load->view('frontend/template/html-header',$dados);
+		$this->load->view('frontend/template/header');
+		$this->load->view('frontend/home');
+		$this->load->view('frontend/template/aside');
+		$this->load->view('frontend/template/footer');
+		$this->load->view('frontend/template/html-footer');
+	}
+
+	
+}
